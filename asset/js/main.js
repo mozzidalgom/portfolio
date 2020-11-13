cbe6a8c43baeb8b9c8c04aa4955308ae4f849098
neodahl_dev_win <neodahl90@gmail.com>
2020-11-13 16:02:16 +0900

    init

---
+++ asset/js/main.js
@@ -0,0 +1,25 @@
+/* Nav Menu Controls */
+(function(global, document , $){
+  'use strict'
+
+ 
+
+})(window, window.document, jQuery);
+
+
+
+
+
+/* Focus Event */
+// (function(global, document , $){
+//   $(global).on('keyup', function(e){
+//     if(e.keyCode == 9) {
+//       $(document.activeElement).addClass('focused');
+//       console.log(document.activeElement);
+//     }
+//   })
+// })(window, window.document, jQuery);
+
+
+
+
