c1215427d7be6af99e82e4dc541b47aad56d51a0
neodahl_dev_win <neodahl90@gmail.com>
2020-12-12 21:52:17 +0900

    section01 완료

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,8 +6,9 @@
 	var section01_TL = gsap.timeline();
 
 	section01_TL
-		
-		.from('.section01 .bg_container .moon', {opacity:0, y:30, duration:2.5}, 0.7) 
+		.fromTo('.section01 .bg_container .moon', 
+			{opacity:0, y:30},
+			{opacity:1, y:0, duration:2.5}, 0.7) 
 		.addLabel('moonRised')
 		.fromTo('.section01 .bg_container .left_tree', 
 			{opacity:0, y:5, scale:0.9, rotation:5, transformOrigin:'left bottom'}, 
@@ -15,14 +16,19 @@
 		.fromTo('.section02 .bg_container .top_right_tree', 
 			{opacity:0, x:50, scale:0.8, rotation:-5}, 
 			{opacity: 1, x:0, rotation:0, scale:1, duration:1.5}, 'moonRised-=1.5')
-		.from('.section01 .img_conatainer', {opacity:0, y:30, duration:1.5}, '-=0.7') 
-		.from('.section01 h1 small, .section01 h1 span, .section01 .title_contaienr > small',
-		{stagger:0.2, duration:1, opacity:0, y:10}, '-=0.5')
-		.from('.section01 .right_msg', {opacity:0, y:10, duration:1}, '-=0.5')
+		.fromTo('.section01 .img_conatainer',
+			{opacity:0, y:30},
+			{opacity:1, y:0, duration:1.5}, '-=.8')
+		.fromTo('.section01 h1 small, .section01 h1 span, .section01 .title_contaienr > small',
+			{opacity:0, y:10},
+			{stagger:0.2, opacity:1, y:0, duration:1}, '-=.5')
+		.fromTo('.section01 .right_msg', 
+			{opacity:0, y:10},
+			{opacity:1, y:0, duration:1}, '-=.5')
 		.addLabel('petalStart')
 		.fromTo('.section01 .bg_container .petal01', 
 			{opacity:0, x:-50, y:-20, scale:0.9}, 
-			{opacity:1, x:0, y:0, scale:1, duration:1.5})
+			{opacity:1, x:0, y:0, scale:1, duration:1.5}, 'petalStart-=.5')
 		.fromTo('.section01 .bg_container .petal02', 
 			{opacity:0, x:-30, y:-60, scale:0.9}, 
 			{opacity:1, x:0, y:0, scale:1, duration:1.5}, 'petalStart+=0.3')
